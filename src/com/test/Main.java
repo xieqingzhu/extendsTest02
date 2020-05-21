5250a9d439fbb1d99d35a4a1dc46213b04f0fe67
xieqingzhu <48080910+xieqingzhu@users.noreply.github.com>
2020-05-21 22:46:15 +0800

    update number

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Student stu = new Student();
         stu.setAge(18);
         stu.setName("java");
-        stu.setNumber(33);
+        stu.setNumber(34);
         stu.message();
         stu.print();
         System.out.println(stu.i);
